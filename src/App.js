cd8f111f4d0796d37063965c10733dae4db3e088
Kyle Bushnell <kyle.bushnell@hotmail.com>
2023-03-29 22:21:48 -0700

    Changes made on 3/29/2023

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ function App() {
   return(
     <>
      <div className='headerPart'>
-     <header>Header</header>
+     <header>
+     <img src="littlelemon2.png" alt="Lemon Logo" width="300" height="200"></img><a href="#">Little Lemon Resturant</a>
+     </header>
      </div>
      <div className='navPart'>
      <nav>
-      Nav Bar
+      <img src="hamburger-menu-512.png" alt="hamburger menu icon"></img>
       <ul>
         <li><a href="/home">Home</a></li>
         <li><a href="/about">About</a></li>
@@ -23,42 +25,83 @@ function App() {
       </ul>
      </nav>
      </div>
-    <div className='mainPart'>
-    <main>Main</main>
+
+    <div className='main'>
+      <div className="mainContainer">
+        <div className="mainRow">
+          <div className="main-col">
+            <main>Main</main>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className='footerPart'>
-    <footer>
-     <ul>
-       <h3>Doormat Navigation</h3>
-       <li><a href="/home">Home</a></li>
-       <li><a href="/about">About</a></li>
-       <li><a href="/menu">Menu</a></li>
-       <li><a href="/reservations">Reservations</a></li>
-       <li><a href="/order_online">Order Online</a></li>
-       <li><a href="/login">Login</a></li>
-       <br></br>
-       <h3>Contact Us!</h3>
-       <br></br>
-       <div>example@gmail.com
-         <br></br>
-           000-000-0000
-           <br></br>
-           Address
-       </div>
-       <br></br>
-       <h3>Social Media Links</h3>
-       <div>
-         <a href="linkedin">LinkedIn</a>
-         <br></br>
-         <a href="twitter">Twitter</a>
-         <br></br>
-         <a href="youtube">YouTube</a>
-         <br></br>
-       </div>
-     </ul>
+    <div className='main'>
+      <div className="mainContainer">
+        <div className="mainRow">
+          <div className="main-col">
+            <main>Main</main>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='main'>
+      <div className="mainContainer">
+        <div className="mainRow">
+          <div className="main-col">
+            <main>Main</main>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='main'>
+      <div className="mainContainer">
+        <div className="mainRow">
+          <div className="main-col">
+            <main>Main</main>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    <footer className="footer">
+      <div className="footerContainer">
+        <div className="row">
+          <div className="footer-col">
+            <h4>Doormat Navigation</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Menu</a></li>
+              <li><a href="#">Reservation</a></li>
+              <li><a href="#">Order Online</a></li>
+              <li><a href="#">Login</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Contact Us</h4>
+            <ul>
+              <li><a href="#">example@gmail.com</a></li>
+              <li><a href="#">000 000-0000</a></li>
+              <li><a href="#">Address</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Social Media Links</h4>
+            <ul>
+              <li><a href="#">LinkedIn</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">YouTube</a></li>
+            </ul>
+            <div className="footer-col">
+              <ul>
+              <li><img src="littlelemon2.png" alt="Lemon Logo" width="300" height="200"></img></li>
+              </ul>
+              </div>
+          </div>
+        </div>
+      </div>
     </footer>
-    </div>
-    </>
+   </>
   );
 }
 
