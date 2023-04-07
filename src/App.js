14afc56e1daafc55d669a7db16989f648532d23b
Kyle Bushnell <kyle.bushnell@hotmail.com>
2023-04-06 20:43:00 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -35,8 +35,8 @@ function App() {
               <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdncjosdnjocnsdjoncj</p>
               <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdncjosdnjocnsdjoncj</p>
               <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdncjosdnjocnsdjoncj</p>
-              <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdncjosdnjocnsdjoncj</p>
-              <img src="restauranfood.png" width="350" height="500"></img>
+              <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdnc</p>
+              <img src="restauranfood.png" width="425" height="600"></img>
               <br></br>
               <button>Free Coupon Code!</button>
             </main>
@@ -47,20 +47,86 @@ function App() {
 
     <div className='main-2'>
       <div className="mainContainer-2">
-        <div className="mainRow-2">
-          <div className="main-col-2">
-            <main>Main</main>
+      <h1>This Weeks Specials!</h1>
+      <button>Order Delivery!</button>
+        <div className="main-2-box-1">
+          <p><h3>Spaghetti $10.99</h3>
+            enkowenfwefeknf
+            kiwenmdjowenfweofn
+            dkwnedfoewnfowe
+            ewonfowenfonwefwen
+            wldnwednjewnf
+          </p>
+          <img src="spaghetti.png" className="Pasta" width="295" height="215"></img>
           </div>
-        </div>
+          <div className="main-2-box-2">
+            <p><h3>Greek Salad $12.99</h3>
+              ejdnoiwendwdweo
+              ewoknkowenkowef
+              ekpwmdkwemfdewn
+              ewkpmdwkpemd
+            </p>
+            <img src="greek salad.png" className="salad" width="295" height="215"></img>
+            </div>
+            <div className="main-2-box-3">
+              <p><h3>Lemon Cake Desert $7.99</h3>
+                smckosandcipsm
+                pksamckpsmncs
+                ojsdnfonfefk
+                pasmdioefniewfn
+              </p>
+              <img src="lemon dessert.png" className="cake" width="295" height="215"></img>
+            </div>
       </div>
     </div>
 
     <div className='main-3'>
+    <h1><a href="#">Testimonials</a></h1>
       <div className="mainContainer-3">
-        <div className="mainRow-3">
-          <div className="main-col-3">
-            <main>Main</main>
-          </div>
+        <div className="main-3-box-1">
+          <img src="Jeremey.png" className="Jeremey" width="248" height="238"></img>
+          <p>The food is some of
+            the best I've ever had,
+            I'd highly recommend!
+
+            5/5 stars
+
+            - Jeremey
+          </p>
+        </div>
+        <div className="main-3-box-2">
+          <img src="Vanessa.png" className="Vanessa" width="248" height="238"></img>
+          <p>Plenty of vegan friendly
+            options, which is always
+            a plus when I'm on the 
+            way to work!
+
+            5/5 stars
+
+            - Vanessa
+          </p>
+        </div>
+        <div className="main-3-box-3">
+        <img src="Mark.png" className="Vanessa" width="248" height="238"></img>
+        <p>Nice place, I'd go again
+
+          4/5 stars
+
+          - Mark
+        </p>
+        </div>
+        <div className="main-3-box-4">
+        <img src="Lily.png" className="Vanessa" width="248" height="238"></img>
+        <p>The waiters were super
+          friendly to me and my kids.
+          That's what I look for the
+          most when I'm trying new 
+          places.
+
+          5/5 stars
+
+          - Lily
+        </p>
         </div>
       </div>
     </div>
@@ -69,7 +135,16 @@ function App() {
       <div className="mainContainer-4">
         <div className="mainRow-4">
           <div className="main-col-4">
-            <main>Main</main>
+            <main>
+            <h1>Little Lemon Resturant</h1>
+            <h2>Chicago</h2>
+              <p>cojsdncjosdnjocnsdjoncjnsaiodncoasncojansncjoancojanconaosnj</p>
+              <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdncjosdnjocnsdjoncj</p>
+              <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdncjosdnjocnsdjoncj</p>
+              <p>odjsniosnocnsdoncjosdncjosdncjonsdjoncojsdnc</p>
+              <img src="restaurant.png" className="kitchen" width="200" height="300"></img>
+              <img src="marioandadrian.png" className="thedude" width="200" height="300"></img>
+            </main>
           </div>
         </div>
       </div>
